4cf22753affc6be0e67cbe00cc47890c835dd55b
surekhad96 <surekhasugan96@gmail.com>
2017-06-08 15:28:27 +0530

    Create prime_interval2.c

--- prime_interval2.c
+++ prime_interval2.c
@@ -2,10 +2,10 @@
 int main()
 {
 int a,b i,flag;
-printf("Enter two numbers(intervals): ");
-scanf("%d%d",&a,&b;
-printf("Prime numbers between %d and %d are:",a,b;
-while(a<b
+printf("Enter two numbers:");
+scanf("%d%d",&a,&b);
+printf("Prime numbers between %d and %d are:",a,b);
+while(a<b)
 {
 flag = 0;
 for(i=2;i<=a/2;++i)
